aa264677937bb09ae03e3efad6f1e787b4b3d6e1
Nikhil Kumar <69901234+NikKumar811@users.noreply.github.com>
2026-07-22 08:53:28 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,8 +4,8 @@ print("Loading trained model...")
 
 model = joblib.load("model.pkl")
 
-#sample = [[5.1, 3.5, 1.4, 0.2]]
-sample = [[6.7, 3.0, 5.2, 2.3]]
+sample = [[5.1, 3.5, 1.4, 0.2]]
+#sample = [[6.7, 3.0, 5.2, 2.3]]
 prediction = model.predict(sample)
 
 species = {
